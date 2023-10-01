fec5175d2af23c9d5d8cb382ea4434958e7b1887
Emaxpm <140901128+Emaxpm@users.noreply.github.com>
2023-10-01 18:40:55 +0000

    Proyecto-generador-de-nombres

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function domainGenerator() {
+  let pronoum = ["The", "Ours", "My", "Yours", "Her", "His"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let adj = [
+    "Big",
+    "Small",
+    "Large",
+    "Medium",
+    "Inteligent",
+    "Cute",
+    "Great",
+    "Amazing"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let noun = [
+    "Duck",
+    "Dog",
+    "Cat",
+    "Lion",
+    "Mouse",
+    "Driver",
+    "Killer",
+    "Dolphin",
+    "Elephant",
+    "Fly",
+    "Racoon"
+  ];
+
+  let generator = [];
+
+  for (let i = 0; i < pronoum.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        generator.push(pronoum[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+  return generator;
+}
+
+// eslint-disable-next-line no-console
+console.log(domainGenerator());
